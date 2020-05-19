6c244aaeb05be6527d55baf199d04391696da71f
s3713572 <s3713572@student.rmit.edu.au>
2020-05-20 00:17:59 +1000

    fix remember digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ApplicationRecord
 
   def remember
     self.remember_token= User.new_token
-    update_attribute(:remember_digest, remember_token)
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 
   def authenticated?(remember_token)
