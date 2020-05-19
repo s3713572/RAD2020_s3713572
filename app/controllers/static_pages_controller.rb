d71119f0444042e639cbdf7778681040b13db547
s3713572 <s3713572@student.rmit.edu.au>
2020-05-20 05:06:17 +1000

    basic level

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,14 @@ class StaticPagesController < ApplicationController
     @microposts = Micropost.where(created_at:(Time.now-1.month..Time.now))
                       .order(created_at: :desc)
     @users = User.order(created_at: :desc).limit(12)
+    @topics= ['A.I.','V.R.','RMIT','Game','Rails','Go','Web','M.L.','Joke','Reat','Vue','Node','iOS','AWS']
+    @top_posts = Micropost.order(view_count: :desc).limit(5)
+    @links = {'Alex':'',
+              'RMIT':'https://www.rmit.edu.au/',
+              'Andy':'',
+              'Rails':'https://rubyonrails.org/',
+              'Docs':'https://guides.rubyonrails.org/'
+    }
   end
 
   def help
